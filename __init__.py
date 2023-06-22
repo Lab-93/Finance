bb3b0525717a4fb242a2bc87760e07887ab2f30f
Hunter Dale <guyyatsu@gmail.com>
2023-06-22 13:08:44 -0400

    Commenting, spacing.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,19 @@
+#!/bin/python3
+"""
+"""
+
+
+# Standard Library imports.
 from datetime import datetime, timedelta
 from time import sleep
 
 
-# Lab-93 Modules
+# In-House module imports.
+# TODO: Replace with a built-in system.
 from Lab93_DatabaseSystem import AdministratorDatabase
 
-# Local Imports
+
+# Local sub-modules.
 from .graphing.CandlestickGraphs import drawCandlestick
 from .data import PriceData
 from .data.historic import Queries
@@ -13,14 +21,19 @@ from .account import AccountDetails
 from .trading import TradeBroker
 
 
+# Establish connection with the credential database.
+# NOTE: This is where we get our API credentials from.
 AdminDB = AdministratorDatabase()
 
 
+# Set datetime constants.
+# NOTE: This is used for establishing a filepath to store generated assets.
 today = datetime.today()
 yesterday = today - timedelta(days=1)
 
 
-# Establish connection to the broker API
+# Establish connection to the broker API.
+# This sets the account client for collecting data on our own holdings.
 AlpacaAPI = AccountDetails(
     ( AdminDB.Retrieve( user = "admin",
                         platform = "alpaca_key" ),
@@ -44,9 +57,12 @@ def Ticker():
 
 TradingBroker = TradeBroker(AlpacaAPI.client)
 
-    
 
 class GraphingReports:
+    """
+    This is the first Top-Level implementation of the graphing submodule; where we can access the underlying
+    functionality in the form of a command-line API.
+    """
     def __init__(self, start=yesterday, end=today,
         output = "/server/front-end/assets/data-science/reports" ):
 
